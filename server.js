18b24d1097d94dbd5407e7214163f3dba8e88db7
David <davidalmeida1620@gmail.com>
2023-08-15 15:29:06 -0500

    Se realizo el Proyecto integrador

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const { tasks } = require('./index.js');
-const bodyParser = require("body-parser")
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
+
+const users = [
+    { email: "davidejemplo@gmail.com", name: "nombre", rol: "admin",password:"12345"  },
+    { email: "jenifer@gmail.com", name: "jenifer", rol: "user", password:"123456" }
+  ];
 
 const app = express();
 
@@ -15,12 +21,51 @@ const validateHttpsMethods = (req,res,next) =>{
 }
 
 app.use(validateHttpsMethods);
-app.use(bodyParser.json())
+app.use(express.json())
 
 const editRouter = require("./routers/list-edit-router.js")
 const viewRouter = require("./routers/list-view-router.js")
 
-app.get("/tasks", (req,res) => {
+app.post("/login",(req,res)=>{
+ const {email,password} = req.body;
+
+ if(!email || !password) return res.status(400).json({error:"No se encontro la informacion"});
+
+ const user = users.find((user) => user.email === email);
+
+ if(!user) return res.status(400).json({error:"Usuario no encontrado en la base de datos"});
+
+ if(user.password !== password) return res.status(401).json({error:"Contraseña incorrecta"});
+
+ const payload = {
+    email: user.email,
+    name: user.name,
+    rol: user.rol
+ }
+
+ const token = generateToken(payload);
+ 
+ res.header("authorization",token).json("todo bien");
+})
+
+function generateToken(payload){
+return jwt.sign(payload,process.env.SECRET_KEY,{algorithm:"HS256",expiresIn:"10m"});
+}
+
+function JWTValidation(req,res,next){
+     const accessToken = req.headers["authorization"];
+
+     if(!accessToken) return res.status(401).json({error:"Token no encontrado"});
+
+     jwt.verify(accessToken, process.env.SECRET_KEY, (err, decodedToken) => {
+        if (err) return res.status(400).json({ error: "Token expirado o clave erronea" });
+        req.decodedToken = decodedToken;
+        next();
+      });
+}
+
+
+app.get("/tasks",JWTValidation, (req,res) => {
     res.json(tasks)
 })
 
